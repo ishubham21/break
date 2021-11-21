619522aa4095e092c69f48d5b2c35ebeb0152e35
ishubham21 <sg2199203@gmail.com>
2021-11-21 23:03:07 +0530

    error handler for smaller screens

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,21 @@
 import styles from './Home.module.css'
-// import { Grid } from '@mui/material'
+import Grid from '@mui/material/Grid'
 
 const Home = () => {
+    
+    //making sure that the window is opened only in the desktop window
+    const width = window.innerWidth
+    if(window.innerWidth < 800){
+        return(
+            <div className={styles.smallScreenMsg}>
+                Please open this window on a desktop
+            </div>
+        )
+    }
+
     return(<>
             <div className={styles.container}>
-
+                <div className={styles.subContainer}></div>
             </div>
         </>
     )
