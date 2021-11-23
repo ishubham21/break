28493d9e977ae8d8e2bf03ccfe67baf0a5c7d42e
ishubham21 <sg2199203@gmail.com>
2021-11-23 13:33:19 +0530

    added side-drawer

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom'
 import CodeBlock from '../CodeBlock/CodeBlock'
 import Navbar from '../Navbar/Navbar'
 import styles from './Dashboard.module.css'
+import { Drawer, Box, AppBar } from '@mui/material'
 
 const Dashboard = () => {
+
+    const drawerWidth = 340 //for MUI drawer
 
     //setting a loading msg before the content of the dashboard loads
     const [loadingMsg, setLoadingMsg] = useState("Crunching your awesome codes, please wait!")
@@ -34,7 +37,7 @@ const Dashboard = () => {
             .then(res => res.json())
             .then(({ error, data }) => {
                 console.log(data)
-                
+
                 const hasError = error != null      //checking for errors returned from the API
                 if (!hasError) {                      //setting values incase of no errors 
                     setLoadingMsg(null)
@@ -57,20 +60,50 @@ const Dashboard = () => {
 
             {userData && <div className={styles.wrapper}>
 
-                <Navbar userName={userData.user.name} logout={logout}/>
+                <Box sx={{ display: 'flex', height: '100%' }}>
+                    <AppBar
+                        position="fixed"
+                        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+                    >
+                    </AppBar>
+                    <Drawer
+                        sx={{
+                            width: drawerWidth,
+                            flexShrink: 0,
+                            '& .MuiDrawer-paper': {
+                                width: drawerWidth,
+                                boxSizing: 'border-box',
+                                backgroundColor: '#5663F7',
+                                color: '#fff',
+                                height: '100%'
+                            },
+                            height: '100%'
+                        }}
+                        variant="permanent"
+                        anchor="left"
+                    >
+                        <Navbar userName={userData.user.name} logout={logout} />
+                    </Drawer>
+                    
+                    <Box
+                        component="main"
+                        sx={{ flexGrow: 1, p: 3 }}
+                    >
+                        {userCodes && <div>
+                            <p className={styles.codesHead}>My Codes</p>
+                            <div className={styles.subContainer}>
+                                {userCodes.map(codeDetails => (
+                                    <CodeBlock fileName={codeDetails.fileName} codeId={codeDetails._id} userId={codeDetails.userId} key={codeDetails._id} />
+                                ))}
+                            </div>
+                        </div>}
 
-                {userCodes && <div>
-                    <p className={styles.codesHead}>My Codes</p>
-                    <div className={styles.subContainer}>
-                        {userCodes.map(codeDetails => (
-                            <CodeBlock fileName={codeDetails.fileName} codeId={codeDetails._id} userId={codeDetails.userId} key={codeDetails._id} />
-                        ))}
-                    </div>
-                </div>}
+                        <Link to='/dashboard/code'>
+                            New
+                        </Link>
 
-                <Link to='/dashboard/code'>
-                    New
-                </Link>
+                    </Box>
+                </Box>
 
             </div>}
         </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
 import styles from './Navbar.module.css'
+import { Button } from '@mui/material'
 
 const Navbar = ({ userName, logout }) => {
     return (<>
-        <nav className={`d-flex align-items-center ${styles.navbar} ${styles.subContainer}`}>
+        <nav className={`${styles.navbar} ${styles.subContainer}`}>
 
             <div className={styles.headingBlk}>
                 <h4 className={styles.brand}>Break</h4>
@@ -11,7 +12,7 @@ const Navbar = ({ userName, logout }) => {
 
             <div className={`d-flex justify-content-center align-items-center`} style={{ marginLeft: 'auto' }}>
                 <h6 >Welcome, {userName}</h6>
-                <button className="btn btn-outline-danger mx-3" onClick={() => { logout() }}>Logout</button>
+                <Button variant="contained" type="submit" onClick={() => { logout() }} className={styles.logoutBtn}>Logout</Button>
             </div>
         </nav>
     </>)
